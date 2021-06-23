bc8f188043defe82792864a1f2299131956ec677
Zer0ni <zeroni70@gmail.com>
2021-06-23 15:42:08 +0300

    Laravel starter pack

---
+++ resources/views/pagewrapper.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <link href="/css/app.css" rel="stylesheet">
+</head>
+<body>
+<!--
+TODO тут подтыкать контент.
+-->
+
+<script src="/js/app.js"></script>
+</body>
+</html>
